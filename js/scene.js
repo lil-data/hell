93d5e0d573090c1b2672ef9b38b1b9f83ce34a1d
Jack Armitage <jack.armitage@me.com>
2015-10-26 21:23:00 +0000

    Basic graveyard;

--- js/scene.js
+++ js/scene.js
@@ -1,22 +1,108 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var container;
+var camera, controls, scene, renderer;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+init();
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+animate();
 
-camera.position.z = 5;
+function init()
+{
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera.position.y = 1;
+	camera.position.z = 5;
+	
+	controls = new THREE.OrbitControls( camera );
+	controls.addEventListener( 'change', render );
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x86877F, 0.2 );
+
+	light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 1, 1, 1 );
+	scene.add( light );
+
+	light = new THREE.DirectionalLight( 0x002288 );
+	light.position.set( -1, -1, -1 );
+	scene.add( light );
+
+	light = new THREE.AmbientLight( 0x222222 );
+	scene.add( light );
+	
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(scene.fog.color, 1);
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container = document.getElementById( 'container' );
+	container.appendChild( renderer.domElement );
+	
+	var texture = THREE.ImageUtils.loadTexture( "img/ground.jpg" );
+	texture.wrapS = THREE.MirroredRepeatWrapping;
+	texture.wrapT = THREE.MirroredRepeatWrapping;
+	texture.repeat.set( 16, 16 );
+
+	var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+	var floorGeometry = new THREE.PlaneGeometry(100, 100, 100, 100);
+	var floor = new THREE.Mesh(floorGeometry, material);
+	floor.rotateX(Math.PI / 2);
+	floor.position.y = 0;
+	scene.add(floor);
+
+	addTombstone(10);
+	
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function addTombstone(amount)
+{
+	var tombstone = new THREE.Shape();
+	var texture = THREE.ImageUtils.loadTexture( "img/granite.jpg" );
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set( 1, 1 );
+
+	tombstone.moveTo( 0, 0, 0 );
+	tombstone.lineTo( 0, 2, 0);
+	tombstone.bezierCurveTo( 0.2, 2.5, 1.3, 2.5, 1.5, 2 );
+	tombstone.lineTo( 1.5, 0, 0);
+
+	var extrudeSettings = {
+		amount: 0,
+		curveSegments: 100,
+		bevelEnabled: true,
+		bevelSegments: 2,
+		steps: 2,
+		bevelSize: 0,
+		bevelThickness: 0.1
+	};
+
+	for (var i = 0; i < amount; i++) {
+		for (var j = 0; j < amount; j++) {
+			var geometry = new THREE.ExtrudeGeometry( tombstone, extrudeSettings );
+			var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({map: texture}) );
+			mesh.position.x = i * 5 - amount * 2;
+			mesh.position.z = j * 5 - amount * 2;
+			scene.add(mesh);
+		}
+	}
+}
+
+function animate()
+{
+	requestAnimationFrame( render );
+	controls.update();
+}
 
 function render() {
-	requestAnimationFrame( render );
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+
+
 	renderer.render( scene, camera );
 }
 
-render();
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  render();
+}
